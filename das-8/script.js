7a2dc7068491862d31dc30507b5cdaed5a12b318
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:45:53 +0400

    array

---
+++ das-8/script.js
@@ -0,0 +1,77 @@
+let str="Barev bolotin, inchpes eq"
+
+
+// str=str.split(" ").reverse().join(" ")
+// console.log(str)
+// str.indexOf() gtnum er daxic aj inch tvel enq ete cher gtnum -1
+// str.includes() gtnum er daxic aj inch tvel enq ete cher gtnum false
+// console.log(str.slice(0,7))
+// let arr=str.split(" ")//['Barev', 'bolotin,', 'inchpes', 'eq']
+//
+// arr.push("vonces")
+// arr.push("lav")
+// arr.push("apres")
+//
+//
+// arr.splice(1,3,"dhez")
+// arr.splice(2,1,"vonceq")
+// arr.splice(4,1,"shnorhakalutyun")
+// // arr.push(false)
+// arr.push(true)
+// arr.push(null)
+// arr.push(undefined)
+// arr.push(NaN)
+// arr.push(1)
+// str=arr.join(" ")// zangvacy sarqec string
+// console.log(str)
+// console.log(arr[arr.indexOf("inchpes")])//2
+// console.log(arr) //
+// +""
+// ""+7 // "7"
+
+// let y=[10,20,30,40,50,60,70,80,90]
+// y[0]  // 7
+// y[1] // 9
+// y[2] // 15
+
+// y.push(36) //avelacnum er verjic himnakan zangvacy poxum er
+// y.unshift(77) //avelacnum er skzbic himnakan zangvacy poxum er
+// y.shift()  // jnjum er arajin andamy
+//  y.pop() // jnjum er verjic mek hat
+// y.reverse()
+// let k=y.slice(2)// slice ktrum e vortexic vortex bayc himnakan zangvacy chi poxum
+// y.splice(2,1,9999)
+
+
+// console.log(y)
+
+// let x=[[45,17,33,25],[2,4,8,10,16]]
+// x[0].push(11)
+// x[1].push(12)
+// console.log(x)
+//
+// // tnayin
+// let arr=[7,8,95,5,5,"55",2,4,8,10,15,undefined,2,null,5,false,55,45,17,33,25,true,34,NaN]
+// console.log(arr.length)
+
+// 1.47 toxi zangvaci arjeqnery gumarel tvery
+// 2.47 toxi zangvaci arjeqnery aradzncnacel tver zugeri ev kenteri //[[45,17,33,25],[2,4,8,10,15]]
+// 3.47 toxi zangvaci arjeqnery barcracnel qarakusi astichan (**2) //[49,65,58595,25,25,95555,...]
+// 4.47 toxi zangvaci arjeqnery veradarcnel chkrknvoxnery//[7,95,4,10,15....]
+
+
+
+// let x=[]
+// while (true){
+//     let p=prompt()
+//     if(!p){
+//         break
+//     }
+//     x.push(p)
+//     console.log(x)
+// }
+
+
+
+
+
