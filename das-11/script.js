69cff7849832c3163606705553c74e48210c626a
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:48:12 +0400

    array 3

---
+++ das-11/script.js
@@ -0,0 +1,99 @@
+// let x=["x","o",3,4,5,6,7,8,9]
+// for(let i=x.length-"x";i>=-"x";i--){
+//     if(i==-"x"){
+//         x[0]="Barev"
+//     }else{
+//         x[i+"x"]=x[i]
+//     }
+// }
+// console.log(x)
+
+// let matrix=[
+//     ["o","x","o","x"],
+//     ["x","x","o","o"],
+//     ["o","o","o","x"],
+//     ["o","x","o","x"],
+// ]
+// let row=0
+// let [one,two]=[0,0]
+// let res="chaxtec vochmek"
+// for (let i=0;i<matrix.length;i++){
+
+//     if(matrix[i][row]===undefined){
+//         break
+//     }
+//
+//
+//     if(matrix[i][row]==="x"){
+//         one++
+//     }else if(matrix[i][row]==="o"){
+//         two++
+//     }
+//
+//     if (matrix.length-1==i){
+//         row++
+//
+//         i=-1
+//         if(one===matrix.length){
+//             res="haxtec x @"
+//             break
+//         }else if(two===matrix.length){
+//             res="haxtec o @"
+//             break
+//         }
+//         two=0
+//         one=0
+//
+//
+//         continue
+//     }
+//
+//
+//
+// }
+// console.log(one)
+// console.log(two)
+// console.log(res)
+
+// unenq prompt gri varkyan 180600 iran veradarcni
+// qani or   2 or
+// qani jam  2 jam
+// qani rope 9r
+
+// 10/4 //2.5
+// 10%4 //2
+// let v=prompt("greq varkyan",180600)
+//
+// let d=parseInt(v/3600/24)
+// let h=parseInt(v/3600%24)
+// let r=parseInt(v/3600%24*60 % 60 )
+//
+// console.log(d+" or")
+// console.log(h+" jam")
+// console.log(r+" roppe")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
