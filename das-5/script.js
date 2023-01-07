d4a1c20ffb4e631db6e30bd4583ba9208225a470
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:43:22 +0400

    while

---
+++ das-5/script.js
@@ -0,0 +1,78 @@
+// indexOf  skzbic gtnum er tari vor error andamn e
+// Lastindexof verjic er gtnum te vor andamn e
+
+let str="barev vonces lav du vonces ipsum dolor sit amet, consectetur vonces elit. Facilis odit praesentium sunt velit! Aliquam commodi culpa dolorem doloribus explicabo laudantium maiores mollitia porro vonces totam. Ea earum excepturi vonces obcaecati.\n"
+
+// str.indexOf("vonces")
+// str.lastIndexOf("vonces")
+// result //barev vonces lav du
+// let res=str.lastIndexOf("vonces")
+// let res2=str.slice(0,res)
+
+// console.log(res2)
+// let res1=str.indexOf("vonces")
+// let res2=str.indexOf("vonces",res1+1)
+// console.log(str.slice(0,res2))
+
+
+// str=str.slice(0,str.indexOf(",")).toUpperCase()+str.slice(str.indexOf(","))
+//
+// console.log(str)
+
+//
+// // y - es
+// // l- s
+// car-- s  => cars
+// pay -- plyes
+//
+
+// let Name = prompt('Write your name ', '');
+//
+// if (Name.slice(-1) === 'y' ){
+//     alert(`${Name}es`)
+// } else {
+//     alert(`${Name}s`)
+// }
+// regexp
+
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+// console.log(11)
+// console.log(12)
+// console.log(13)
+// console.log(14)
+// console.log(15)
+// console.log(16)
+// console.log(17)
+let i=0;
+let p=prompt("greq tiv")
+while (i<=p){
+    console.log(i)
+    i++
+}
+// sax promptov grel tivy
+// 1.tpi miayn zuyg tvery
+// 2.tpi miayn kent tvery tvery
+// **************
+// 3. hashvi zuyg tveri gumary orinak pr //8 inq ta 2+4+6+8==20
+// 4. prompti mej grel ays devachapov 3,5 hashvi ayd tveri ynkac gumary3+4+5
+// 5.  prompti mej anun gre shur ta hakarak orinak// Mane veradarcni Enam
+
+// nayel
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+// https://www.youtube.com/watch?v=rIdt_RPOLFQ&t=1046s
+
+
+
+
+
