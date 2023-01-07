e663bd5771101de6e22169b446be24342256836c
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:49:45 +0400

    array and function

---
+++ das-12/script.js
@@ -0,0 +1,150 @@
+document.write(`<table border='1' width='50%'>`)
+for (let i=2;i<=9;i++){
+    document.write(`<tr>`)
+    for (let j=2;j<=9;j++){
+        document.write(`<td>${i}x${j}=${i*j}</td>`)
+    }
+    document.write(`</tr>`)
+}
+document.write(`</table>`)
+
+
+
+
+
+
+
+// 1
+// let student=[
+//     ["Ani","Mels","Karen"],
+//     ["Ando","Artsom","Samvel"],
+//     ["Samvel","Entoni","Mika","Petros"],
+//     ["Sergey","Martun","Artur"],
+//     ["Ani","Anna","Mxitar","Inga"],
+// ]
+
+// 2
+// let st=["Ani","Mels Gyumri","Karen","Ando","Artsom","Samvel","Samvel","Entoni","Mika","Petros","Sergey","Martun","Artur"]
+// unenal porompt vornir te umnes cankanum
+// ni ["Ani","Entoni"]
+
+
+
+// Mxitar  | Grigor | Petros
+//   1          1      1
+//
+
+// let saySome = prompt()
+// let st=["Ani","Mels Gyumri","Karen","Ando",
+//     "Artsom","Samvel","Samvel","Entoni","Mika",
+//     "Petros","Sergey","Martun","Artur"];
+// let empty = [];
+//
+// for(let i=0;i<st.length;i++)
+// {    if(st[i].toLowerCase().includes(saySome.toLowerCase())){
+//
+//         empty.push(st[i])
+//     }
+// }
+
+// console.log(empty)
+// let text = prompt('write your text').trim().toLowerCase()
+// let arr =["Ani","Mels Gyumri","Karen","Ando","Artsom","Samvel","Samvel","Entoni","Mika","Petros","Sergey","Martun","Artur"]
+// let arr2 = []
+//
+// for (let i = 0;i<arr.length;i++){
+//     if (arr[i].toLowerCase().indexOf(text)!=-1){
+//         arr2.push(arr[i])
+//     }
+// }
+// console.log(arr2)
+
+// push
+// shift
+// unshift
+// slice
+// splice
+// reverce
+// join
+// pop
+
+// foreach
+// map
+// filter
+// reduce
+// find
+// sort
+
+let arr =["Ani","Mels Gyumri","Karen","Ando","Artsom","Samvel",
+    "Mika", "Samvel","Entoni","Petros","Sergey","Martun","Artur"]
+
+
+
+// console.log(a)
+// arr.forEach(function (res,i){
+//     console.log(a)
+// })
+
+// let x=arr.map(function (res,i){
+//     if(res=="Mels Gyumri"){
+//         return "Mels"
+//     }
+//     return res
+// })
+// console.log(x)
+
+// let text = prompt('write your text').trim().toLowerCase()
+//
+// let fil=arr.filter((res)=>res.toLowerCase().includes(text))
+
+// console.log(fil)
+
+
+
+
+let a=[10,20,30,40,60]
+// let sum=a.reduce((previousValue, currentValue)=> {
+//     return previousValue*currentValue
+// },1)
+// console.log(sum)
+
+
+// let f=a.find((res)=>res==10)
+//
+// console.log(f)
+
+
+// function f1() {
+//
+// }
+//
+// let f2=function () {
+//    return 1
+// }
+//
+//
+// f2()
+
+// let x=()=> 1
+// let x=()=> {
+// return 1
+// }
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+// tnayinery mnum e nuyn@
+// locel nor methodnerov
+
+// https://nicepage.com/website-templates/preview/we-build-stores-529408?device=desktop
+
+
+
+
+
+
+
+
+
+
