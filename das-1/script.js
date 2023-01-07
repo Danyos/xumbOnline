bc417e9125a358eb23de0ff63721b77069d412d1
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:25:51 +0400

    javascript first lesson

---
+++ das-1/script.js
@@ -0,0 +1,108 @@
+// alert(window)
+// alert(2)
+// alert(3)
+// console.log(window)
+// console.log(2)
+// console.log(3)
+// document.write(window);
+// document.write(2);
+// console.log(5+5)
+// console.log(5*5)
+
+// let x=5
+// let y=8
+// let z=0
+// z=x+y//13
+// z=z+1 //14
+// z*=2
+// let c=x*y //40
+// c*=2//80
+// console.log(c)
+
+// let FullName=1
+// let full_name=2
+// let fullname
+// let fullname_2
+// console.log(x)
+
+// let arkx=15
+// arkx+=10
+//
+// arkx+=1
+// console.log(arkx)
+// arkx=arkx-5
+// console.log(arkx)
+//
+//
+// arkx=36
+//
+// console.log(arkx)
+//      / * - +           ** %
+
+// arkx=arkx**2// astichan
+// arkx=arkx%6
+
+// let arkx=15
+// console.log(arkx)
+
+//
+// ******************TYPE************************
+
+// String " " ' ' alert("barev bolorin")
+// Number 123456789 /*-+ ** %
+// Boolean true false
+// object
+// null
+// undefined
+
+// let t=null
+
+// console.log(t)//null
+// console.log(undefined===null)
+// let x=7
+// let y="7"
+// console.log(x===y)
+// typeof(undefined) 'undefined'
+// Boolean() false
+// Boolean(undefined) false
+
+// false arjeqner
+// !!undefined
+// !!null false
+// !!'' false
+// !!0 false
+// !!NaN false
+
+// > <
+// >= <=
+// let num=1
+
+
+// num+=1
+// num++
+// ++num
+// console.log(num++)
+// console.log(num)
+// console.log(num)
+// let num5=(7+5)*2//24
+// https://learn.javascript.ru/types
+// https://learn.javascript.ru/alert-prompt-confirm
+// https://learn.javascript.ru/type-conversions
+// https://learn.javascript.ru/operators
+// https://learn.javascript.ru/comparison
+
+// algoritmi tesutyun
+
+
+
+
+
+
+
+
+
+
+
+
+
+
