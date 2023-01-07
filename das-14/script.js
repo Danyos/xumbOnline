dc019b58951d0025cac7278b4ec9fb590d0dd7d0
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:52:03 +0400

    object 2

---
+++ das-14/script.js
@@ -0,0 +1,278 @@
+const accountList=[
+    {
+        id:1,
+        name:"Valod",
+        payment_way:"visa",
+        price:"1500$",
+        exchange:"usd",
+    }, {
+        id:2,
+        name:"Vazgen",
+        payment_way:"visa",
+        price:"1400$",
+        exchange:"amd",
+    }, {
+        id:3,
+        name:"grish",
+        payment_way:"bitcoin",
+        price:"1700$",
+        exchange:"usd",
+    }, {
+        id:4,
+        name:"Karo",
+        payment_way:"visa",
+        price:"100$",
+
+    }, {
+        id:5,
+        name:"Karo",
+        payment_way:"idram",
+        price:"800$",
+
+    },
+]
+
+
+
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+
+// let sum=arr.reduce((previousValue,currentValue)=>{
+//
+//     return previousValue*currentValue
+// },1)
+// console.log(sum)
+
+//
+// let fl=arr.filter(res=>res%2===0)
+//    let f2=fl.map(res=>res*2)
+//     let f3=f2.reduce((previousValue,currentValue)=>{
+//
+//   if (currentValue%2===0 && currentValue%4===0){
+//       previousValue.push(currentValue)
+//   }
+//   return previousValue
+// },[])
+// console.log(f3)
+//
+//
+// let f4=arr.reduce((previousValue,currentValue)=>{
+//
+//     if(currentValue%2===0){
+//         if ((currentValue*2) % 2 ===0 && (currentValue*2) % 4===0){
+//             previousValue.push(currentValue*2)
+//         }
+//     }
+//     return previousValue
+// },[])
+// console.log(f4)
+
+
+
+
+
+
+
+// let stantart=arr.reduce((previousValue,currentValue)=>{
+//
+//   if (currentValue%2===0){
+//       previousValue.push(currentValue)
+//   }
+//   return previousValue
+// },[])
+// console.log(stantart)
+
+
+
+
+
+
+
+
+
+
+
+
+// let valueMoney=accountList.filter(res=>res.exchange==="amd")
+// console.log(valueMoney)
+
+//
+//
+// let allMoney = {}
+
+
+// accountList.forEach(function (account)
+// {
+//     if(allMoney[account.payment_way] === undefined )
+//     {
+//         allMoney[account.payment_way] = parseInt(account.price);
+//     }else{
+//         allMoney[account.payment_way]+=parseInt(account.price)
+//     }
+//
+//
+// })
+// console.log(allMoney);
+
+// let obj={
+//     id:4,
+//     name:"Karo",
+//     payment_way:"visa",
+//     price:"100$"
+// }
+// let a="payment_way"
+// console.log(obj[a])
+// console.log(obj.price)
+
+// let user=[
+//     {
+//         id:1,
+//         name:"poghos",
+//         surname:"Poghosyan",
+//         login:"poxos",
+//         psw:"123456",
+//         status:true,
+//     },
+//     {
+//         id:2,
+//         name:"Arman",
+//         surname:"Poghosyan",
+//         login:"arman",
+//         psw:"123456",
+//         status:true,
+//     },
+//     {
+//         id:3,
+//         name:"Gegham",
+//         surname:"Poghosyan",
+//         login:"Gegam",
+//         psw:"123456",
+//         status:true,
+//     },
+//     {
+//         id:4,
+//         name:"Hrant",
+//         surname:"Poghosyan",
+//         login:"hranto",
+//         psw:"123456",
+//         status:false,
+//     },
+//     {
+//         id:5,
+//         name:"Garsevan",
+//         surname:"Poghosyan",
+//         login:"garso",
+//         psw:"123456",
+//         status:true,
+//     }
+// ]
+//
+//
+//
+// let login=prompt("Mutqagre dher login")
+// let psw=prompt("Mutqagre dher password")
+//
+//
+// let findUser=user.find(function (res){
+//     return  res.login===login && res.psw===psw
+// })
+//
+// if(findUser && findUser.status===true){
+//     alert("barev")
+// }else if(findUser && findUser.status===false){
+//     alert("duq arqelapakvac ogtatern eq")
+// }else{
+//     alert("nman ogtater goyutyun chuni")
+// }
+
+
+
+//
+// let obj={
+//     visa:300
+// }
+// let a="idram"
+// let b="taradram"
+//
+// if(obj[a]===undefined){
+//     obj[a]=100
+// }else{
+//     obj[a]+=200
+// }
+// console.log(obj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// tnayin
+
+// https://code.mu/en/javascript/book/prime/objects/entire-object-display/
+
+// let arr=["a","c","k","c","e","f","c","c","d","c","f","c","c","d","e","f"]
+// hashvel te vor antamic qani hat ka veradarcneq object orinak
+
+// a__1
+// c__5
+// d__44
+
+
+
+
+// const accountList=[
+//     {
+//         id:1,
+//         name:"Valod",
+//         payment_way:"visa",
+//         price:"1500$",
+//         exchange:"usd",
+//     }, {
+//         id:2,
+//         name:"Vazgen",
+//         payment_way:"visa",
+//         price:"1400$",
+//         exchange:"amd",
+//     }, {
+//         id:3,
+//         name:"grish",
+//         payment_way:"bitcoin",
+//         price:"1700$",
+//         exchange:"usd",
+//     }, {
+//         id:4,
+//         name:"Karo",
+//         payment_way:"visa",
+//         price:"100$",
+//
+//     }, {
+//         id:5,
+//         name:"Karo",
+//         payment_way:"idram",
+//         price:"800$",
+//
+//     },
+// ]
+
+
+
+// reduse lucel ays xndiry
+
+
+
+
+
+
+
+
+
+
+
