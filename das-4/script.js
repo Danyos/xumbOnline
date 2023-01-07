c088cc4c018fca527123300264aac95b084fccde
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:42:42 +0400

    ternar operator

---
+++ das-4/script.js
@@ -0,0 +1,121 @@
+// && ||
+// ==
+// ===
+//>=
+//<=
+// let x="7"
+// switch (x){
+//     case "5":
+//         console.log(5)
+//         break
+//     case "6":
+//         console.log(6)
+//         break
+//     case "7":
+//         console.log(7)
+//         break
+//     case "8":
+//         console.log(8)
+//         break
+//     default:console.log("sxal")
+//
+// }
+// // unenq 3 prompt 1,tiv 2 mej gortcoxutyun nshan 3 tiv
+//
+//
+//
+//
+
+// let FirstNum = +prompt('Write Number');
+// let Sign = prompt('Write + or - or / or * ');
+// let SecondNum = +prompt('Write Number');
+// let result = 0;
+// //
+// if (!isNaN(FirstNum) && !isNaN(SecondNum)) {
+//     switch (Sign) {
+//         case '+':
+//             result = FirstNum + SecondNum;
+//             break
+//         case '-':
+//             result = FirstNum - SecondNum;
+//             break
+//         case '/':
+//             result = FirstNum / SecondNum;
+//             break
+//         case  '*':
+//             result = FirstNum * SecondNum;
+//             break
+//         default:
+//             alert('Please Write Correct');
+//     }
+// }else {
+//     alert('Please Write Correct');
+// }
+// alert(result);
+//
+
+// let msg
+// if('+'){
+//     msg="barev"
+// }else if(1!==1){
+// msg="hi"
+// }else if(+"hajox"){
+//     msg="h"
+// }
+// else{
+//     msg="1111111"
+// }
+// console.log(msg)
+//
+//
+// let msg=(5>9 )?"barev"
+//     :(1!==1) ? "hi"
+//         :+"hajox" ? "h"
+//             :"1111"
+// console.log(msg)
+
+
+
+
+// let car="bmw"
+//
+// let carModel=car==="kia"?"Kia":car==="Lexus"?"Lexus gx":car=="bmw"?"Bmw x5":"chgtnvats meqena"
+// console.log(carModel)
+//
+
+// tnayin
+
+// tnayin cody ksarqeq ternar operatorov
+// 1 xndir
+// let a=7
+// let b=15
+// let c=25
+// gtnel amenamecy
+
+// 2 xndir
+// let message;
+//
+// if (login == 'garik23' && psw == 'Valodya23') {
+//
+//     message = 'Привет';
+// } else if (login == 'Samvel123' && psw == 'Muxi123') {
+//
+//     message = 'Здравствуйте';
+// } else if (login == '' && psw =='muxiis') {
+//     message = 'Нет логина';
+// }else{
+//     message = '';
+// }
+
+
+
+// 3 rd xndir
+// let result;
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+
+
+
