f33c1c1111aa8464295a223cf224c5c46bd4818d
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:44:22 +0400

    for

---
+++ das-6/script.js
@@ -0,0 +1,198 @@
+// let x=prompt("greq mijakayq")
+// let i=1
+// let res=0
+// while(i<=x){
+//
+//     if(i%5===0 && i%3===0){
+//         console.log(i)
+//         res+=i
+//     }
+//     i++
+// }
+// console.log(res)
+
+// let x2=prompt("greq mijakayq")
+// let start=x2.slice(0,x2.indexOf(","))
+// let end=x2.slice(x2.indexOf(",")+1)
+// let i=start
+// while(i<=end){
+// console.log(+i)
+//     i++
+// }
+
+
+//
+// let anun=prompt("greq anun")
+// let res=""
+// let i=anun.length-1 //2
+// while (i>=0){
+//     res+=i==anun.length-1?anun[i].toUpperCase():anun[i].toLowerCase()
+//     i--
+// }
+//
+// console.log(res)
+
+
+// let i=0
+// do{
+//     console.log(i)
+//     i++
+//
+// }while (i<5)
+
+
+//
+// for (let i=0;i<5;i++){
+//     console.log(i)
+// }
+// let anun=prompt("greq anun")//"Karo"//
+// let res=""
+// for (let i=anun.length-1;i>=0;i--){
+//     res+=anun[i]
+// }
+// console.log(res)
+//
+//
+// let Num = prompt('write your number');
+
+// let str="barev - vonces - laves - ha - du - vonces"
+// str[0]="k"
+// console.log(str)
+// for(let i = 0; true;i++){
+//     if(str.includes("-")){
+//         // ........
+//     }else{
+//         break
+//     }
+//
+//
+// }
+
+
+
+
+
+
+// 4.unenal prompt vori mej gres tiv, tpel minchev 1000@ chgerazancox tvi qarakusin
+//         5 ---> 25,125,625
+
+// 5. unenal 2 prompt vortex 1 promptum kgres te vor tvic sksi
+//   2 rd promptum vortex avartvi ayd hatvacum ynkac tvery tpel
+// 5.1 hashvel dranc aranq tveri gumary
+// 5.2 hashvel zuyg tveri gumary
+
+
+//  tnayin
+
+// 1.unenal 2 prompt vori mej kgres tivy 2 rd prompti mej astichan@
+//  aranc ** ogtagorcman hashvel tvi qarakusin
+
+// 2. unenal prompt vori mej tiv greluc khashvi factoryal@ orinak. mut exac tivy 6 e veradarcni
+// 1*2*3*4*5*6
+
+// 3. stexcel aynpisi cikl vor kashxati hakarak orinak sovorakan devy 1,2,3,4,5,6,7,8,9....
+// mez petq e tpel 9,8,7,6,5,4,3,2,1
+
+// 4. unenal prompt vori mej gres anun shur ta hakarak ogtagortcel cikl
+
+
+
+
+// let num = 1;
+// let tiv=10
+//
+// for(let i = 0; i < 10; i)
+// {
+//
+//     if(num*tiv > 1000)
+//     {
+//        break
+//     }
+//         num *= tiv
+//
+//
+// }
+// console.log(num);
+//
+//
+//
+
+
+
+
+
+// let TheNum = prompt('write number');
+// let NumDegree = prompt('write number degree');
+// let result = 1;
+//
+// for (let i = 0; i < NumDegree;i++){
+//     result *=  TheNum
+//
+// }
+// alert("Բարև")
+
+
+// let Name = prompt('Write your name');
+// let result = true;
+//
+// for (let i = 0;i < Name.length;i++){
+//     if (Name.charCodeAt(i) <= 1415 && Name.charCodeAt(i) >= 1329){
+//
+//     } else{
+//         result = false
+//         break
+//     }
+// }
+// alert(result)
+
+
+
+// 2
+// azganun grvac miayn hayeren u ete azagnuni mej
+// gtsik ka stugel ayn chlni bari skzbum kam vergum kam 1 ic aveli
+
+
+
+
+
+// document.write("<table class='tb' border='1'>")
+// for (let i=0;i<8;i++){
+//     document.write("<tr>")
+//     for (let j=0;j<8;j++){
+//        if((i+j)%2==0){
+//            document.write(`<td style="background: red">`)
+//            document.write("</td>")
+//        }else{
+//            document.write(`<td style="background: green">`)
+//            document.write("</td>")
+//        }
+//     }
+//     document.write("<tr>")
+// }
+// document.write("</table>")
+
+// 1
+// stexcel axusyak
+// matemi
+// 2x1=2
+// 2x2=4
+// ....
+// 2x9=18
+
+// minchev 9 axusyaky
+// kam teblov
+
+
+document.write("barev\t&nbsp;\t&nbsp;\t&nbsp;\t&nbsp;\t&nbsp;\t&nbsp;\t&nbsp;vonces")
+
+
+
+
+
+
+
+
+
+
+
+
