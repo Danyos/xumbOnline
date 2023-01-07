6dae80767e7a9996c6749d524121089ec2a70621
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-07 23:33:13 +0400

    if

---
+++ das-2/script.js
@@ -0,0 +1,135 @@
+let num="1440"
+let num2=60
+// console.log(num+num2)
+// string+string=stringstring
+// string+number=stringstring
+
+// number string sarqel
+// let str=""+num2
+// let str2=num2.toString()
+// let str3=String(num2)
+// console.log(str3)
+
+// let num1=+num
+// let num_2=parseInt(num)
+// let num_3=parseFloat(num)
+// let num_4=num*1
+// console.log(num_4)
+// +"45.44445544" // 45.44445544
+// parseInt("18 tarekan") // 18
+// parseInt("aa5555ed5ws5d5s") // NaN
+
+
+
+
+
+
+// undefined===undefined
+// true
+
+// false===false
+// true
+
+// null==null
+// true
+
+// 0===0
+// true
+
+// NaN===NaN
+// false
+
+// typeof NaN
+// 'number'
+
+// +"125dew"===NaN
+// false
+
+
+// let age="-18"
+// console.log(+age>0 || +age<0 )
+// NaN brnel dever
+
+    //1. +age>0
+    // 2.!isNaN(age)
+// console.log(!isNaN(parseInt(age)))
+
+
+
+
+// 7<7 || 1<10 || 1==7
+// true
+// false || null || undefined || 0
+// 0
+// !!0
+// false
+// false || null || true || undefined || 0
+// true
+// true && 5<7 && 4==4 && "Barev"
+// 'Barev'
+// true && true && 4==4 && "Barev"
+// 'Barev'
+// true && false || 4!=4 && "Barev"
+// false
+
+
+
+
+// let x=parseInt(prompt("greq tiv"))
+// let y=prompt("gre anun")
+// console.log(x)
+
+// let balans_kashilok=1000
+// let balans_visa=2000
+// let product=1500
+// if(balans_kashilok>=product){
+//     balans_kashilok-=product
+//     alert("vcharumy katarvac e qo kashilok")
+// }else if(balans_visa>=product){
+//     balans_visa-=product
+//     alert("vcharumy katarvac e qo qartic")
+// }else{
+//     alert("dzher gumary chbavararec")
+// }
+//
+//
+// if(balans_kashilok>=product || balans_visa>=product){
+//     alert("vcharumy katarvac ")
+// }else{
+//     alert("dzher gumary chbavararec")
+// }
+// let result=+prompt()
+// if (typeof result === 'string'){
+//
+//     document.write(result);
+// } else{
+//     alert('Error');
+// }
+
+
+// unenal  3 prompt
+// 1>in mej gre tiv
+// 2> mej grel ortcoxutyan nshan
+// 3> grelk tiv
+// greluc heto hashvi irenc arjeqy
+// orinak
+
+// 1  56
+// 2  "+"
+// 3  94
+// resul 150
+// if(){
+//     // if(arjeq2=="+"){
+// //    console.log( arjeq1+arjeq3)
+// // }
+// }else{
+//
+// }
+
+
+// 2 xndir
+
+// prompti mej harcnel ogtatiroj tariqy ete 18 ic cacr e asel duq der shat poqr eq
+// ete 18-63 tarekani mijakayqum asi duq barehajox mutq gortceciq 63+ asi duq shat mets eq
+
+
