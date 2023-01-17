c1bfc47aad12267fa1f18bdc0c47865517a7f18a
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-17 23:27:24 +0400

    dom element

---
+++ das-18/index.js
@@ -0,0 +1,93 @@
+// function randomColor(){
+//     let red=Math.ceil(Math.random() * 255)
+//     let green=Math.ceil(Math.random() * 255)
+//     let blue=Math.ceil(Math.random() * 255)
+//     return `rgb(${red},${green},${blue})`
+// }
+// function changeColor(){
+//     let actualDiv = document.querySelector(".cub")
+//     actualDiv.style.background=randomColor()
+// }
+
+
+
+
+
+
+// document.body.style.backgroundColor=randomColor()
+//
+//
+//
+// let x=document.querySelector(".info")
+// x.innerHTML+=`<li>${randomColor()}</li>`
+//
+//
+
+//
+// let count=0
+// function res() {
+//     let x = document.querySelector('.info')
+//     let info_text=document.querySelector('.info_text')
+//     if(info_text.value.trim().length>0){
+//         x.innerHTML += `<li>${info_text.value +" "+ count++ }</li>`
+//         info_text.value=""
+//     }
+//
+// }
+//
+
+
+
+// tnayin
+
+// const price=[
+//     {
+//         id:1,
+//         name:"Valod",
+//         payment_way:"master",
+//         price:"1500$"
+//     }, {
+//         id:2,
+//         name:"Vazgen",
+//         payment_way:"paypal",
+//         price:"1400$"
+//     }, {
+//         id:3,
+//         name:"grish",
+//         payment_way:"idram",
+//         price:"1700$"
+//     }, {
+//         id:4,
+//         name:"Karo",
+//         payment_way:"visa",
+//         price:"100$"
+//     }, {
+//         id:5,
+//         name:"Serob",
+//         payment_way:"bitcoin",
+//         price:"500$"
+//     }, {
+//         id:6,
+//         name:"Petros",
+//         payment_way:"visa",
+//         price:"800$"
+//     }
+// ]
+
+
+// console.log(price[price.length-1].id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
